098c1c7cba7d293143a31552d939ab3a2db28b08
Veeramani A <veeramaniamk@gmail.com>
2025-06-11 20:45:30 +0530

    initial commit

---
+++ collections/LinkedListPractice.java
@@ -0,0 +1,12 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class LinkedListPractice {
+
+    public static void main(String[] a) {
+        LinkedList linkedList = new LinkedList<>();
+        
+    }
+
+}
