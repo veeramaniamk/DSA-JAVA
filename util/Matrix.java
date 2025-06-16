91529e2cc78ee5ba21cce17438e7ade5ee7c0a07
Veeramani A <veeramaniamk@gmail.com>
2025-06-16 20:41:52 +0530

    nothing

--- util/Matrix.java
+++ util/Matrix.java
@@ -2,28 +2,33 @@ package util;
 
 public class Matrix {
 
-
-
     public void addition(int[][] a, int[][] b) throws Exception {
 
         if(a.length == 0 || b.length == 0) throw new IllegalStateException("Empty Matrix Found");
 
         boolean itIsPossibleToCalculate = true;
         int firstMatrixRowCount = a.length;
-        int secondMatrixRowCount = 0;
-        int firstMatrixColumnCount = 0;
-        int secondMatrixColumnCount = 0;
+        int secondMatrixRowCount = b.length;
+        int firstMatrixColumnCount = a[0].length;
+        int secondMatrixColumnCount = b[0].length;
 
         System.err.println("a.length -> "+ firstMatrixRowCount + " a[0].length -> " + a[0].length);
 
         for(int i=0;i<firstMatrixRowCount;i++) {
-            System.err.println( "dd "+a[i].length);
             if(a[0].length!=a[i].length) throw new IllegalStateException("First matrix column values are not same");
         }
 
         for (int index = 0; index < b.length; index++) {
-            
+            if(b[0].length!=b[index].length) throw new IllegalStateException("Second matrix column values are not same");
         }
+
+        if(firstMatrixRowCount != secondMatrixRowCount) throw new IllegalStateException("Given matrix row values are not matching");
+
+        for(int i=0;i<firstMatrixRowCount;i++) {
+            if(a[i].length!=b[i].length) throw new IllegalStateException("First and Second matrix column values are not matching");
+        }
+
+        
 
     }
     
