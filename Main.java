b18c93b1cf3700de5b5a1cebe7ab2b8f564a1dce
Veeramani A <veeramaniamk@gmail.com>
2025-07-01 23:21:53 +0530

    sprial matrix practice failed

--- Main.java
+++ Main.java
@@ -29,17 +29,19 @@ public class Main {
 
         System.out.println(size);
 
-        try {
-          int[][] mat = matrix.generateSpiralMatrix(1000);
-          for (int[] row : mat) {
-            for (int val : row) {
-                System.out.printf("%3d ", val);
-            }
-            System.out.println();
-          }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        // try {
+        //   int[][] mat = matrix.generateSpiralMatrix(1000);
+        //   for (int[] row : mat) {
+        //     for (int val : row) {
+        //         System.out.printf("%3d ", val);
+        //     }
+        //     System.out.println();
+        //   }
+        // } catch (Exception e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+        spiral(25);
         
     }
 
@@ -52,9 +54,41 @@ public class Main {
 
         int left = 0, right = size - 1;
         int top = 0, bottom = size - 1;
-        int number = 0;
+        int number = 1;
 
         int[][] result = new int[size][size];
+
+        while(number<=size) {
+
+            for(int i=left;i<=right && number<=size;i++) {
+                result[left][i] = number++;
+            }
+            top++;
+
+            for(int i=top;i<=bottom && number<=size;i++) {
+                result[i][right] = number++;
+            }
+            right--;
+
+            for(int i=right;i>=left && number<=size;i--) {
+                result[bottom][i] = number++;
+            }
+            bottom--;
+
+            for(int i=bottom;i>=top && number<=size;i--) {
+                result[left][i] = number++;
+            }
+            left++;
+
+        }
+
+        int[][] mat = result;
+          for (int[] row : mat) {
+            for (int val : row) {
+                System.out.printf("%3d ", val);
+            }
+            System.out.println();
+          }
 
     }
 
