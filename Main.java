6d3d8d01309ab2b727820fba12f43fb0a3fab713
veeramani <veeramaniamk@gmail.com>
2025-07-07 12:24:16 +0530

    addition matrix are deleted

--- Main.java
+++ Main.java
@@ -1,28 +1,7 @@
 
-import util.Numbers;
-
 public class Main {
     
     public static void main(String[] args) {
-       AdditionMatrix additionMatrix = new AdditionMatrix();
-
-        int[][] a = {{1,2,3}, {4,5,6}};
-        int[][] b = {{1,2,3}, {4,5,6}};
-
-       try{
-        int[][] re = additionMatrix.matrixAddition(a, b);
-
-        for(int i=0;i<re.length;i++) {
-            for(int j=0;j<re[i].length;j++) {
-                System.out.print(re[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-       } catch(Exception e) {
-         System.out.println(e.getMessage());
-       }
-
        
     }
 
