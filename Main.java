5ba7c717ed08d099025743f82eeb5960b4e37d32
veera <veeramaniamk@gmail.com>
2026-04-16 09:35:17 +0530

    nothing

--- Main.java
+++ Main.java
@@ -1,8 +1,45 @@
 
 public class Main {
-    
-    public static void main(String[] args) {
-       
+
+    public static void main(String[] args) throws Exception {
+        Bird a = new Dog();
+        a.sound();
     }
 
+}
+
+interface Animal {
+    void sound();
+}
+
+interface Bird {
+    default void sound(int a) {
+        System.out.println("Bird sound");
+    }
+}
+
+class Dog implements Bird, Animal {
+    public void sound() {
+
+    }
+}
+
+
+class Singleton {
+    private static  Singleton instance;
+
+    private Singleton() {
+        System.out.println("Instance created by: " + Thread.currentThread().getName());
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
